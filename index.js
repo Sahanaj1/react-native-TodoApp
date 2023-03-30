5925876bcecbfe82da48857f57d520e4a7c89bd5
sahana joshi <sahanajoshi07@gmail.com>
2023-03-30 10:25:54 +0530

    created todoapp

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-import "expo-router/entry";
+import { View,Text} from "react-native";
+
+
+const Home=()=>{
+    return (
+        <View>
+        <Text>Home</Text>
+        </View>
+    )
+}
+export default Home;
